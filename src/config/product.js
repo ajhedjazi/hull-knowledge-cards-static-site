50aaa167c90449b1fbcad3492005fd023ad45cc1
ajhedjazi <167655647+ajhedjazi@users.noreply.github.com>
2026-08-01 20:08:57 +0100

    Add commercial product configuration

---
+++ src/config/product.js
@@ -0,0 +1,8 @@
+export const PRODUCT = {
+  name: "Hull Knowledge Cards",
+  priceDisplay: "£19.99",
+  accessDays: 90,
+  checkoutUrl: "",
+  disclaimer:
+    "Hull Knowledge Cards is an independent revision resource. It is not affiliated with or endorsed by Hull City Council. Questions are provided for revision purposes and are not official council examination questions.",
+};
