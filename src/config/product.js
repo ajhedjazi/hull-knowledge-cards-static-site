b823b0cd23672893dffbb760df1b9e4e58eae873
ajhedjazi <167655647+ajhedjazi@users.noreply.github.com>
2026-08-02 11:52:50 +0100

    Wire Stripe sandbox payment link for commercial MVP

--- src/config/product.js
+++ src/config/product.js
@@ -2,7 +2,7 @@ export const PRODUCT = {
   name: "Hull Knowledge Cards",
   priceDisplay: "£19.99",
   accessDays: 90,
-  checkoutUrl: "",
+  checkoutUrl: "https://buy.stripe.com/test_7sY7sK0SY5Oy30e2Yx8AE00",
   disclaimer:
     "Hull Knowledge Cards is an independent revision resource. It is not affiliated with or endorsed by Hull City Council. Questions are provided for revision purposes and are not official council examination questions.",
 };
